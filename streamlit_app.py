f85b275d7ae697f83c3a9c1bcf68c84a2f83ca81
wil-qua <62174303+wil-qua@users.noreply.github.com>
2022-09-05 16:00:37 +0200

    Update streamlit_app.py
    added try and except

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,17 +27,18 @@ streamlit.dataframe(fruits_to_show)
 
 # display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    streamlit.error("Please select a fruit to get information.")
+  else:  
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalized)
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-
-# normalize json version of the response
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-
-# output on the screen as table
-streamlit.dataframe(fruityvice_normalized)
-
+except URLError as e:
+  streamlit.error()
+    
 streamlit.stop()
 
 streamlit.header("The fruit load list contains:")
